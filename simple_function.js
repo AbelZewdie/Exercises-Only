3d2a344c7176798ae80432c30d7a5a3cd0d70379
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-30 15:05:18 -0400

    first commit

---
+++ simple_function.js
@@ -0,0 +1,20 @@
+function favoriteThings(){
+  console.log("Abel")
+  console.log("The GodFather")
+  console.log("The Weeknd")
+}
+favoriteThings()
+
+console.log(" ");
+
+function whyImHere(){
+  console.log("Leaning to Code.")
+}
+whyImHere()
+
+console.log(" ");
+
+function favoritePlace(){
+  console.log("Ethiopia, cause I was born and raised there.")
+}
+favoritePlace()
